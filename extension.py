2238a1e229556e9093e7fbbeced58d7ff8c46b58
Kettle27 <82636700+Kettle27@users.noreply.github.com>
2023-11-01 17:44:46 +0000

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,9 +1,14 @@
 import math
+import numpy as np
+
+
 
 
 class fact:
 
+
     def __init__(self,val):
+
 
         super(fact).__repr__()
 
@@ -27,6 +32,7 @@ class fact:
         
     @classmethod
     def calc_int(cls, val):
+
 
         if val == 1 or val == 0:
 
@@ -140,13 +146,15 @@ class intergration:
 
     def __init__(self, equation, a, b):
 
+
         super(intergration).__repr__()
 
         self.delta_X = (b-a) / 1000
 
         self.x_range = [a + (i * self.delta_X) for i in range(1001)]
 
-        self.trap_sum = sum([2 * eval(equation, {"x": x}) for x in self.x_range[1:-1]], eval(equation, {"x" : self.x_range[0]}) + eval(equation, {"x" : self.x_range[-1]}))
+        self.trap_sum = sum([2 * eval(equation, {"x": x}) for x in self.x_range[1:-1]]
+        , eval(equation, {"x" : self.x_range[0]}) + eval(equation, {"x" : self.x_range[-1]}))
 
         
     def __repr__(self):
@@ -161,4 +169,140 @@ class intergration:
 
 
 
+class log:
 
+
+    def __init__(self,val):
+
+
+        super(log).__repr__()
+
+        self.val = val
+
+        if self.val > 0:
+
+            self.log = np.log(self.val)
+        
+        else:
+
+            self.log = 0
+        
+        
+    def __repr__(self):
+
+        return f"{self.log}"
+    
+
+    def  __float__(self):
+
+        return float(self.log)
+    
+
+    def __add__(self, other):
+
+        if isinstance(other, log):
+
+            return self.log + other.log
+        
+        elif isinstance(other, (int, float)):
+
+            return self.log + other
+    
+
+    def __radd__(self, other):
+
+        return self + other
+    
+
+    def __mul__(self,other):
+
+        if isinstance(other, (log)):
+
+            return self.log * other.log
+        
+        if isinstance(other, (int, float)):
+
+            return self.log * other
+    
+
+    def __rmul__(self, other):
+
+        return self * other
+        
+
+    def __sub__(self, other):
+
+        if isinstance(other, log):
+        
+            return self.log + (-other.log)
+        
+        elif isinstance(other, (int, float)):
+
+            return self.log + (-other)
+        
+    
+    def __rsub__(self, other):
+
+        return (self * -1) + other
+    
+
+    def __truediv__(self, other):
+
+        if isinstance(other, log):
+        
+            return self.log / other.log
+        
+        elif isinstance(other, (int, float)):
+
+            return self.log / other
+    
+
+    def __rtruediv__(self, other):
+
+        return self / other
+     
+    
+    def __pow__(self, other):
+
+        if isinstance(other, log):
+        
+            return self.log ** other.log
+        
+        elif isinstance(other, (int, float)):
+
+            return self.log ** other
+
+    
+    def __rpow__(self, other):
+
+        return other ** self.log
+
+
+
+
+class range_x:
+
+
+    def __init__(self, a, b, n):
+
+
+        super(range_x).__repr__()
+
+        self.delta_x = (b-a)/n
+
+        self.x_range = [a + (i * self.delta_x) for i in range(n+1)]
+
+
+    def __getitem__(self, val):
+
+        return self.x_range[val]  
+
+
+    def __repr__(self):
+
+        return f"{self.x_range}"
+    
+
+    def __len__(self):
+
+        return len(self.x_range)
