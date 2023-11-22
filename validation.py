ff1413dce736885fae8039600c0ee822703d0c88
Kettle27 <82636700+Kettle27@users.noreply.github.com>
2023-11-22 03:00:27 +0000

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,11 +1,11 @@
-from tkinter import *
-import sqlite3
-import re
-
-
+from db_handler import *
 
 class App(Frame):
+
+
     def __init__(self,master):
+
+
         super(App, self).__init__(master)
         self.grid()
 
@@ -14,7 +14,6 @@ class App(Frame):
         self.graphingFrame = Frame(self)
         self.signupFrame = Frame(self)
 
-        self.validate = "false"
 
         self.create_login_widgets()
         self.loginFrame.grid()
@@ -31,8 +30,6 @@ class App(Frame):
         self.Password_label = Label(self.loginFrame,text="Password:").grid(row=2,column=0, padx=10, pady=10)
 
 
-
-
         self.username_entry = Entry(self.loginFrame, width=50)
         self.username_entry.grid(row=1, column=1, columnspan=2, padx=5)
 
@@ -41,23 +38,21 @@ class App(Frame):
         self.password_entry.grid(row =2, column=1, columnspan=2,padx=5)
 
 
-
-
         def show():
-
 
             toggle_button.config(text="hide ",command=lambda:hide(),relief="groove")
             self.password_entry.config(show="")
+
+
         def hide():
 
 
             toggle_button.config(text="show ",command=lambda:show(),relief="groove")
             self.password_entry.config(show="*")
    
+
         toggle_button = Button(self.loginFrame,text='show',command=lambda:show(),relief="groove")
         toggle_button.grid(row=2, column=4, padx=5)
-
-
 
 
         login_button = Button(self.loginFrame,text="          Login          ",relief="groove",command=lambda:self.check_login_validation()).grid(row=3,column=1,padx=100)
@@ -66,24 +61,11 @@ class App(Frame):
 
 
     def check_login_validation(self):
-        try:
-            conn = sqlite3.connect("logins.db")
-            cursor = conn.cursor()
 
 
+        if Login.check_login(self.username_entry.get(), self.password_entry.get()):
+                
 
-
-            statement = f"""SELECT * FROM login
-                            WHERE Username = "{self.username_entry.get()}" AND Password = "{self.password_entry.get()}" """
-            cursor.execute(statement)
-
-
-            if cursor.fetchall() == []:
-                self.Error_label.config(text="Invalid Username or Password!",relief="raised",bg="red")
-                self.username_entry.delete(0, END)
-                self.password_entry.delete(0, END)
-               
-            else:
                 self.Error_label.config(text=f"Welcome back {self.username_entry.get()}",relief="raised",bg="green")
                 self.username_entry.delete(0, END)
                 self.password_entry.delete(0, END)
@@ -92,12 +74,17 @@ class App(Frame):
                 self.accept()
                
 
+        else:
 
-        except sqlite3.Error as err:
-            print("An error occured", err)
-        conn.close()
-       
+
+            self.Error_label.config(text="Invalid Username or Password!",relief="raised",bg="red")
+            self.username_entry.delete(0, END)
+            self.password_entry.delete(0, END)
+            
+
     def create_new_login(self):
+
+
         self.loginFrame.grid_remove()
         self.signupFrame.grid()
 
@@ -106,16 +93,12 @@ class App(Frame):
         win.geometry("450x200")
 
 
-
-
         self.Error_label = Label(self.signupFrame,text=" ")
         self.Error_label.grid(row=0, column=1, padx=10, pady=10)
 
 
         self.Username_label = Label(self.signupFrame,text="Username:").grid(row=1, column=0, padx=10, pady=10)
         self.Password_label = Label(self.signupFrame,text="Password:").grid(row=2,column=0, padx=10, pady=10)
-
-
 
 
         self.username_entry = Entry(self.signupFrame, width=50)
@@ -139,59 +122,63 @@ class App(Frame):
             toggle_button.config(text="show ",command=lambda:show(),relief="groove")
             self.password_entry.config(show="*")
    
+
         toggle_button = Button(self.signupFrame,text='show',command=lambda:show(),relief="groove")
         toggle_button.grid(row=2, column=4, padx=5)
            
-        signup_button = Button(self.signupFrame,text='          sign up         ',relief="groove",command=lambda:validate_new_login()).grid(row=3, column=1, padx=100, pady=15)
+
+        signup_button = Button(self.signupFrame,text='          sign up         ',relief="groove",command=lambda:self.validate_new_login()).grid(row=3, column=1, padx=100, pady=15)
 
 
-        def validate_new_login():
-            if re.fullmatch("[A-Za-z0-9]{8,}", self.username_entry.get()) and re.fullmatch("[A-Za-z0-9._%+-^$£!*&]{8,}", self.username_entry.get()) is not None:
-                try:
-                    conn = sqlite3.connect("logins.db")
-                    cursor = conn.cursor()
+    def validate_new_login(self):
 
 
-                    statement = f"""SELECT * FROM login
-                                WHERE Username = "{self.username_entry.get()}" """
-                    cursor.execute(statement)
+        if re.fullmatch("[A-Za-z0-9]{8,}", self.username_entry.get()) and re.fullmatch("[A-Za-z0-9._%+-^$£!*&]{8,}" , self.username_entry.get()) is not None:
 
 
-                    if cursor.fetchall() == []:
-                        statement = f"""INSERT INTO login (Username, Password)
-                                VALUES ("{self.username_entry.get()}","{self.password_entry.get()}")"""
-                        cursor.execute(statement)
-                        conn.commit()
-                        self.signupFrame.grid_remove()
-                        self.loginFrame.grid()
-                        self.create_login_widgets()
-                        win.geometry("450x250")
-                        win.title("Login")
-                   
-                    else:
-                        self.Error_label.config(text="Username already exists!",relief="raised",bg="red")
-                        self.username_entry.delete(0, END)
-                        self.password_entry.delete(0, END)
+            if Login.create_login(self.username_entry.get(), self.password_entry.get()):
 
 
- 
-                except sqlite3.Error as err:
-                    print("An error occured", err)
-                conn.close()
-
-            else:
-                self.Error_label.config(text="Invalid Username or Password!",relief="raised",bg="red")
+                self.Error_label.config(text="Username already exists!",relief="raised",bg="red")
                 self.username_entry.delete(0, END)
                 self.password_entry.delete(0, END)
 
+               
+            else:
+
+
+                self.signupFrame.grid_remove()
+                self.loginFrame.grid()
+                self.create_login_widgets()
+                win.geometry("450x250")
+                win.title("Login")
+
+
+        else:
+
+
+            self.Error_label.config(text="Invalid Username or Password!",relief="raised",bg="red")
+            self.username_entry.delete(0, END)
+            self.password_entry.delete(0, END)
+
+
     def accept(self):
+
+
         win.destroy()
 
 
-class Valid:
-    global win
-    win = Tk()
-    win.geometry("450x250+300+300")
-    app = App(win)
-    win.overrideredirect(True)
-    win.mainloop()
+
+
+
+
+win = Tk()
+win.geometry("450x250+300+300")
+app = App(win)
+win.overrideredirect(True)
+win.mainloop()
+
+
+
+
+#User_info.Add_func("x**2")
