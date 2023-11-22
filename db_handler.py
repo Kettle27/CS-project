75117288be0ea1877ab8038ae53239069918ae4f
Kettle27 <82636700+Kettle27@users.noreply.github.com>
2023-11-22 03:02:08 +0000

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -9,22 +9,103 @@ class Login:
     @staticmethod
     def check_login(Username, Password):
 
+
         conn = sqlite3.connect("Calculator.db")
         cursor = conn.cursor()
 
 
         statement = f"""SELECT * FROM Logins
                     WHERE Username = "{Username}" AND Password = "{Password}" """
+        
+
 
         cursor.execute(statement)
 
-        if cursor.fetchall() == []:
-                
+
+        if cursor.fetchall() == []:    
+
             return False
-               
+
+
         else:
-                
+
+            global user
+            user = Username
+
             return True
+    
+
+    @staticmethod
+    def create_login(Username, Password):
+
+
+        conn = sqlite3.connect("Calculator.db")
+        cursor = conn.cursor()
+
+
+        statement = f"""SELECT * FROM Logins
+                    WHERE Username = "{Username}"; """
         
 
+        cursor.execute(statement)
+
+
+        if cursor.fetchall() == []:
+
+
+            statement = f"""INSERT INTO Logins (Username, Password)
+                        VALUES ("{Username}","{Password}");"""
+            cursor.execute(statement)
+            conn.commit()
+
+            conn.close()
+
+
+        else:
+                
+            conn.close()
+
+            return True
+
+        
+
+class User_info:
+
+    
+    @staticmethod
+    def Load_func():
+        
+        pass
+
+
+    @staticmethod
+    def Add_func(function, x, y):
+
+        conn = sqlite3.connect("Calculator.db")
+        cursor = conn.cursor()
+
+
+        statement = f"""INSERT INTO Graphs (Username, function)
+                    VALUES ("{user}","{function}");"""
+        
+
+        cursor.execute(statement)
+        conn.commit()
+
+        for i in range(2001):
+
+
+            statement = f"""INSERT INTO GraphInfo (function, x_val, y_val)
+                        VALUES ("{function}","{x[i]}","{y[i]}");"""
+
+
+            cursor.execute(statement)
+            conn.commit()
+        
         conn.close()
+    
+
+    @staticmethod
+    def Del_func(function):
+
+        pass
