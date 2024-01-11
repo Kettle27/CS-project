c6a9d4afd4b856b9a15ad6edc3ec4f63a8a5fc65
Kettle27 <82636700+Kettle27@users.noreply.github.com>
2024-01-11 13:36:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ from matplotlib.widgets import Button as Btn
 
 
 from extension import *
-import validation
 from db_handler import *
 
 
@@ -33,6 +32,245 @@ backend_bases.NavigationToolbar2.toolitems = (
     ('Zoom', 'Zoom to rectangle', 'zoom_to_rect', 'zoom'))
 
 
+# Create validation class for logins and sign ups
+
+
+class Val(Frame):
+
+
+    def __init__(self,master):
+        
+
+       # inheritance class
+
+       super(Val, self).__init__(master)
+       self.grid()
+
+
+       # create frames
+
+       self.loginFrame = Frame(self)
+       self.signupFrame = Frame(self)
+
+
+       # initiate login widgets
+
+       self.create_login_widgets()
+       self.loginFrame.grid()
+
+
+
+    # function plots all login widgets   
+
+
+    def create_login_widgets(self):
+
+
+       self.Error_label = Label(self.loginFrame,text=" ")
+       self.Error_label.grid(row=0, column=1, padx=10, pady=10)
+
+
+       self.Username_label = Label(self.loginFrame,text="Username:").grid(row=1, column=0, padx=10, pady=10)
+       self.Password_label = Label(self.loginFrame,text="Password:").grid(row=2,column=0, padx=10, pady=10)
+
+
+       self.username_entry = Entry(self.loginFrame, width=50)
+       self.username_entry.grid(row=1, column=1, columnspan=2, padx=5)
+
+
+       self.password_entry = Entry(self.loginFrame,width = 50, show="*")
+       self.password_entry.grid(row =2, column=1, columnspan=2,padx=5)
+
+
+
+       # show and hide functions change the password from hiden to shown
+
+       def show():
+
+            toggle_button.config(text="hide ",command=lambda:hide(),relief="groove")
+            self.password_entry.config(show="")
+
+
+       def hide():
+
+
+            toggle_button.config(text="show ",command=lambda:show(),relief="groove")
+            self.password_entry.config(show="*")
+   
+
+       toggle_button = Button(self.loginFrame,text='show',command=lambda:show(),relief="groove")
+       toggle_button.grid(row=2, column=4, padx=5)
+
+
+       login_button = Button(self.loginFrame,text="          Login          ",relief="groove",command=lambda:self.check_login_validation()).grid(row=3,column=1,padx=100)
+       or_label = Label(self.loginFrame,text="or").grid(row=4,column=1,pady=10)
+       signup_button = Button(self.loginFrame,text='        sign up         ',relief="groove",command=lambda:self.create_new_login()).grid(row=5, column=1, padx=5)
+
+
+
+    # class method to check if the logins provided are correct
+
+    def check_login_validation(self):
+
+
+        # calls class from different python file to check 
+
+        if Login.check_login(self.username_entry.get(), self.password_entry.get()):
+                
+
+                self.Error_label.config(text=f"Welcome back {self.username_entry.get()}",relief="raised",bg="green")
+                self.username_entry.delete(0, END)
+                self.password_entry.delete(0, END)
+                self.loginFrame.grid_remove()
+                self.graphingFrame.grid()
+                self.accept()
+               
+
+        else:
+
+
+            self.Error_label.config(text="Invalid Username or Password!",relief="raised",bg="red")
+            self.username_entry.delete(0, END)
+            self.password_entry.delete(0, END)
+            
+ 
+    # removes login widgets and places the sign up widgets
+
+    def create_new_login(self):
+
+
+       self.loginFrame.grid_remove()
+       self.signupFrame.grid()
+
+
+       win.title("Sign Up")
+       win.geometry("450x200")
+
+
+       self.Error_label = Label(self.signupFrame,text=" ")
+       self.Error_label.grid(row=0, column=1, padx=10, pady=10)
+
+
+       self.Username_label = Label(self.signupFrame,text="Username:").grid(row=1, column=0, padx=10, pady=10)
+       self.Password_label = Label(self.signupFrame,text="Password:").grid(row=2,column=0, padx=10, pady=10)
+
+
+       self.username_entry = Entry(self.signupFrame, width=50)
+       self.username_entry.grid(row=1, column=1, columnspan=2, padx=5)
+
+
+       self.password_entry = Entry(self.signupFrame,width = 50, show="*")
+       self.password_entry.grid(row =2, column=1, columnspan=2,padx=5)
+
+        
+
+       # show and hide functions change the password from hiden to shown
+
+       def show():
+
+
+            toggle_button.config(text="hide ",command=lambda:hide(),relief="groove")
+            self.password_entry.config(show="")
+
+
+       def hide():
+
+
+            toggle_button.config(text="show ",command=lambda:show(),relief="groove")
+            self.password_entry.config(show="*")
+   
+
+       toggle_button = Button(self.signupFrame,text='show',command=lambda:show(),relief="groove")
+       toggle_button.grid(row=2, column=4, padx=5)
+           
+
+       signup_button = Button(self.signupFrame,text='          sign up         ',relief="groove",command=lambda:self.validate_new_login()).grid(row=3, column=1, padx=100, pady=15)
+
+
+
+    # validates the sign up details using regular expression
+
+    def validate_new_login(self):
+
+
+        if re.fullmatch("[A-Za-z0-9]{8,}", self.username_entry.get()) and re.fullmatch("[A-Za-z0-9._%+-^$£!*&]{8,}" , self.username_entry.get()) is not None:
+
+
+            if Login.create_login(self.username_entry.get(), self.password_entry.get()):
+
+
+                self.Error_label.config(text="Username already exists!",relief="raised",bg="red")
+                self.username_entry.delete(0, END)
+                self.password_entry.delete(0, END)
+
+               
+            else:
+
+
+                self.signupFrame.grid_remove()
+                self.loginFrame.grid()
+                self.create_login_widgets()
+                win.geometry("450x250")
+                win.title("Login")
+
+
+        else:
+
+
+            self.Error_label.config(text="Invalid Username or Password!",relief="raised",bg="red")
+            self.username_entry.delete(0, END)
+            self.password_entry.delete(0, END)
+
+
+
+    # accept state starts the new program
+
+    def accept(self):
+
+       win.destroy()
+
+       global root, app, graph_list
+       root = Tk()
+       app = App(root)
+       graph_list = []
+
+       root.bind("<comma>", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push(","), app.plot_entry.config(state = DISABLED)])
+       root.bind("0", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("0"), app.plot_entry.config(state = DISABLED)])
+       root.bind("1", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("1"), app.plot_entry.config(state = DISABLED)])
+       root.bind("2", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("2"), app.plot_entry.config(state = DISABLED)])
+       root.bind("3", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("3"), app.plot_entry.config(state = DISABLED)])
+       root.bind("4", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("4"), app.plot_entry.config(state = DISABLED)])
+       root.bind("5", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("5"), app.plot_entry.config(state = DISABLED)])
+       root.bind("6", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("6"), app.plot_entry.config(state = DISABLED)])
+       root.bind("7", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("7"), app.plot_entry.config(state = DISABLED)])
+       root.bind("8", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("8"), app.plot_entry.config(state = DISABLED)])
+       root.bind("9", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("9"), app.plot_entry.config(state = DISABLED)])
+       root.bind("<BackSpace>", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.pop(), app.plot_entry.config(state=DISABLED)])
+       root.bind("<period>", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("."), app.plot_entry.config(state = DISABLED)])
+       root.bind("(", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("("), app.plot_entry.config(state = DISABLED)])
+       root.bind(")", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push(")"), app.plot_entry.config(state = DISABLED)])
+       root.bind("!", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("fact"), app.plot_entry.config(state = DISABLED)])
+       root.bind("x", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("x"), app.plot_entry.config(state = DISABLED)])
+       root.bind("y", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("y"), app.plot_entry.config(state = DISABLED)])
+       root.bind("<Return>", lambda x:[app.plot_entry.configure(state = NORMAL), Graph_tools.plot_graph(app.plot_entry.get()), entry_stack.pull(), app.plot_entry.configure(state = DISABLED)])
+
+
+       for x, y, fx in User_info.Load_func("2D"):
+          
+              graph = app.ax.plot(x, y, color = "r")
+                            
+              app.fig_canvas.draw()
+
+              app.mylist.insert(END, f" y = {fx}")
+
+              User_info.Add_func("2D", fx, x, y)
+
+              graph_list.append(graph)
+
+
+       root.mainloop()
+
+
 # Create class called App to be the main Tkinter Frame
 
 
@@ -41,6 +279,8 @@ class App(Frame):
        
        def __init__(self, master):
 
+
+              # inheritance class
 
               super(App, self).__init__(master)
 
@@ -311,12 +551,14 @@ class App(Frame):
                ).place(x=203, y=461)
               
 
-              # below is a Tkinter slider which represents the theta value of when we want to rotate a graph
+              # below is a Tkinter slider and label which represents the theta value of when we want to rotate a graph
 
+              self.slider_label = Label(self.meta_dataFrame, text = "x_axis", font= "Arial 15", bg = "#e8eaed")
+              self.slider_label.place(x=80,y=10)
 
               self.slider = Scale(self.meta_dataFrame, from_=0, to=360, bg = "#5f6368", fg = "#e8eaed", troughcolor= "#5f6368", borderwidth= 3, orient= HORIZONTAL, length= 200, command=
                lambda x :dim2_rotation_matrix())
-              self.slider.place(x=10,y=10)
+              self.slider.place(x=10,y=50)
 
 
               # added Tkinter Scrollbar to scroll on the Listbox if needed
@@ -423,7 +665,7 @@ class Graph_tools(App):
               if str(app.dim_indx_btn.label)[16:18] == "2D":
 
 
-
+                     # checks the database to see if the function has already been done before
                      # the method will try and do the calculations
                      # if it fails it does not plot anything
 
@@ -493,6 +735,7 @@ class Graph_tools(App):
               if str(app.dim_indx_btn.label)[16:18] == "3D":
 
 
+                     # checks the database to see if the function has already been done before
                      # the method will try and do the calculations
                      # if it fails it does not plot anything
 
@@ -565,6 +808,8 @@ class Graph_tools(App):
                                    app.fig_canvas.draw()
 
 
+                                   # then add the equation used and the graph (value for graph is an address)
+
                                    User_info.Add_func("3D", fx, X, Y, Z)
                                    graph_list.append(graph)
 
@@ -577,6 +822,8 @@ class Graph_tools(App):
 
                                    pass
 
+
+       # class staticmethod deletes graphs                           
 
        @staticmethod             
        def delete_graph():
@@ -787,13 +1034,18 @@ class Index(App):
             app.x_slider.destroy()
             app.y_slider.destroy()
             app.z_slider.destroy()
+            app.slider_labelx.destroy()
+            app.slider_labely.destroy()
+            app.slider_labelz.destroy()
 
+
+            app.slider_label = Label(app.meta_dataFrame, text = "x_axis", font= "Arial 15", bg = "#e8eaed")
+            app.slider_label.place(x=80,y=10)
 
             app.slider = Scale(app.meta_dataFrame, from_=0, to=360, bg = "#5f6368", fg = "#e8eaed", troughcolor= "#5f6368", borderwidth= 3, orient= HORIZONTAL, length= 200, command=
              lambda x :dim2_rotation_matrix())
-              
-
-            app.slider.place(x=10,y=10)
+             
+            app.slider.place(x=10,y=50)
 
 
        
@@ -846,27 +1098,31 @@ class Index(App):
 
 
             app.slider.destroy()
+            app.slider_label.destroy()
+
+            app.slider_labelx = Label(app.meta_dataFrame, text = "x_axis", font= "Arial 15", bg = "#e8eaed")
+            app.slider_labelx.place(x=80,y=10)
+            
+            app.slider_labely = Label(app.meta_dataFrame, text = "y_axis", font= "Arial 15", bg = "#e8eaed")
+            app.slider_labely.place(x=300,y=10)
+            
+            app.slider_labelz = Label(app.meta_dataFrame, text = "z_axis", font= "Arial 15", bg = "#e8eaed")
+            app.slider_labelz.place(x=510,y=10)
 
 
             app.x_slider = Scale(app.meta_dataFrame, from_=0, to=360, bg = "#5f6368", fg = "#e8eaed", troughcolor= "#5f6368", borderwidth= 3, orient= HORIZONTAL, length= 200, command=
              lambda x :dim3_rotation_matrix())
-              
-
-            app.x_slider.place(x=10,y=10)
+            app.x_slider.place(x=10,y=50)
 
 
             app.y_slider = Scale(app.meta_dataFrame, from_=0, to=360, bg = "#5f6368", fg = "#e8eaed", troughcolor= "#5f6368", borderwidth= 3, orient= HORIZONTAL, length= 200, command=
              lambda x :dim3_rotation_matrix())
-              
-
-            app.y_slider.place(x=220,y=10)
+            app.y_slider.place(x=220,y=50)
 
 
             app.z_slider = Scale(app.meta_dataFrame, from_=0, to=360, bg = "#5f6368", fg = "#e8eaed", troughcolor= "#5f6368", borderwidth= 3, orient= HORIZONTAL, length= 200, command=
              lambda x :dim3_rotation_matrix())
-              
-
-            app.z_slider.place(x=430,y=10)
+            app.z_slider.place(x=430,y=50)
 
 
 
@@ -1044,44 +1300,8 @@ class dim3_rotation_matrix:
 
 if __name__ == "__main__":
     
-
-    root = Tk()
-    app = App(root)
-
-    root.bind("<comma>", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push(","), app.plot_entry.config(state = DISABLED)])
-    root.bind("0", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("0"), app.plot_entry.config(state = DISABLED)])
-    root.bind("1", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("1"), app.plot_entry.config(state = DISABLED)])
-    root.bind("2", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("2"), app.plot_entry.config(state = DISABLED)])
-    root.bind("3", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("3"), app.plot_entry.config(state = DISABLED)])
-    root.bind("4", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("4"), app.plot_entry.config(state = DISABLED)])
-    root.bind("5", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("5"), app.plot_entry.config(state = DISABLED)])
-    root.bind("6", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("6"), app.plot_entry.config(state = DISABLED)])
-    root.bind("7", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("7"), app.plot_entry.config(state = DISABLED)])
-    root.bind("8", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("8"), app.plot_entry.config(state = DISABLED)])
-    root.bind("9", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("9"), app.plot_entry.config(state = DISABLED)])
-    root.bind("<BackSpace>", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.pop(), app.plot_entry.config(state=DISABLED)])
-    root.bind("<period>", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("."), app.plot_entry.config(state = DISABLED)])
-    root.bind("(", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("("), app.plot_entry.config(state = DISABLED)])
-    root.bind(")", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push(")"), app.plot_entry.config(state = DISABLED)])
-    root.bind("!", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("fact"), app.plot_entry.config(state = DISABLED)])
-    root.bind("x", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("x"), app.plot_entry.config(state = DISABLED)])
-    root.bind("y", lambda x: [app.plot_entry.config(state = NORMAL), entry_stack.push("y"), app.plot_entry.config(state = DISABLED)])
-
-    global graph_list
-    graph_list = []
-
-    for x, y, fx in User_info.Load_func("2D"):
-          
-       graph = app.ax.plot(x, y, color = "r")
-                            
-       app.fig_canvas.draw()
-
-       app.mylist.insert(END, f" y = {fx}")
-
-       User_info.Add_func("2D", fx, x, y)
-
-       graph_list.append(graph)
-
-
-    root.mainloop()
-
+    win = Tk()
+    win.geometry("450x250+300+300")
+    val = Val(win)
+    win.overrideredirect(True)
+    win.mainloop()
